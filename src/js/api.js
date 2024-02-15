289531aac370d1631c95805ccd212cd6125ff592
Hoomam <hoaz1600@student.miun.se>
2024-02-15 15:33:37 +0100

    created charts

--- src/js/api.js
+++ src/js/api.js
@@ -1,12 +1,13 @@
 "use strict"
-
+import Chart from 'chart.js/auto'
+// Hämtar data från API
 async function fetchData() {
     try {
         const response = await fetch('https://studenter.miun.se/~mallar/dt211g/');
         const data = await response.json();
-
         let course = [];
         let program = [];
+        // Loop through och skicka in kurse och program till var sin array
         data.forEach((item)=> {
         if(item.type === "Kurs" ){
             course.push(item);      
@@ -14,6 +15,7 @@ async function fetchData() {
             program.push(item);      
         }
         });
+
         courseChart(course); 
         programChart(program);
         
@@ -27,10 +29,45 @@ fetchData();
 
 function courseChart(course){
     course.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
-    console.log(course.slice(0, 6));
+    let data = course.slice(0, 6);
+    
+    new Chart(
+        document.getElementById('kurs'),
+        {
+          type: 'bar',
+          data: {
+            labels: data.map(row => row.name),
+            datasets: [
+              {
+                label: 'Kurser',
+                data: data.map(row => row.applicantsTotal)
+              }
+            ]
+          }
+        }
+      );    
 }
 
 function programChart(program){
     program.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
-    console.log(program.slice(0, 6));
+    let data = program.slice(0, 6);
+    new Chart(
+        document.getElementById('program'),
+        {
+          type: 'pie',
+          data: {
+            labels: data.map(row => row.name),
+            datasets: [
+              {
+                label: 'Program',
+                data: data.map(row => row.applicantsTotal)
+              }
+            ]
+          }
+        }
+      );    
 }
+
+
+
+
